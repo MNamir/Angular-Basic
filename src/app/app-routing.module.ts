3c13261bc5f32f6e337f259c70d65eb62a302b79
MohdNameer <mohd.nameer@quovantis.com>
2019-10-29 16:02:49 +0530

    add footer link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,39 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { FormComponent } from './form/form.component';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { Child1Component } from './child1/child1.component';
-import { Child2Component } from './child2/child2.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { FormComponent } from "./form/form.component";
+import { HeaderComponent } from "./header/header.component";
+import { Child1Component } from "./child1/child1.component";
+import { Child2Component } from "./child2/child2.component";
+import { FooterComponent } from "./footer/footer.component";
 
 const routes: Routes = [
-  { path:"harshit", component: HeaderComponent},
- {
-   path:"form",
-   component:FormComponent
- },
- {
-   path: "child1", component: Child1Component
- },
- {
-   path: "child2" , component:Child2Component
- },
- {
-   path:"",
-   redirectTo:"/child22",
-   pathMatch: 'full'
- },
- { path: '**', component: Child1Component }
-
-
+  { path: "harshit", component: HeaderComponent },
+  {
+    path: "form",
+    component: FormComponent
+  },
+  {
+    path: "child1",
+    component: Child1Component
+  },
+  {
+    path: "child2",
+    component: Child2Component
+  },
+  {
+    path: "",
+    redirectTo: "/child22",
+    pathMatch: "full"
+  },
+  {
+    path: "footer",
+    component: FooterComponent
+  },
+  { path: "**", component: Child1Component }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
