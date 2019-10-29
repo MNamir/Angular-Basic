b831383ddefaa0b12e8ea6adf4b6f4f8cd2e49b0
MohdNameer <mohd.nameer@quovantis.com>
2019-10-29 13:18:07 +0530

    Angular basic components call and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { Child1Component } from './child1/child1.component';
+import { Child2Component } from './child2/child2.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"harshit", component: HeaderComponent},
+ {
+   path:"form",
+   component:FormComponent
+ },
+ {
+   path: "child1", component: Child1Component
+ },
+ {
+   path: "child2" , component:Child2Component
+ },
+ {
+   path:"",
+   redirectTo:"/child22",
+   pathMatch: 'full'
+ },
+ { path: '**', component: Child1Component }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { Child1Component } from './child1/child1.component';
+import { Child2Component } from './child2/child2.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { PasswordComponent } from './password/password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    HeaderComponent,
+    FooterComponent,
+    Child1Component,
+    Child2Component,
+    LogInComponent,
+    PasswordComponent
   ],
   imports: [
     BrowserModule,
